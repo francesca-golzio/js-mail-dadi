69c4a455f74d1796659d6b8b3a33912448488df0
Francesca Golzio <francescagolzio@gmail.com>
2026-02-05 15:50:03 +0100

    add comments

--- mail/assets/JS/main.js
+++ mail/assets/JS/main.js
@@ -2,6 +2,27 @@
 //console.log('🟢 Yes. it is!');
 
 
+/* DADI */ 
+/* variabili | array | prompt | operatori js | console.log() | for() */ 
+
+/* Creo una lista di invitati (contiene 'email') */ 
+
+/* Chiedo all'Utente la sua 'email' */
+
+/* Controllo se l'email fa parte della lista */
+
+/* Restituisco un messaggio appropriato (ricordo di usare 'for()'*/
+
+/* Restituisco un messaggio appropriato */
+
+
+
+
+
+
+
+
+
 
 
 
