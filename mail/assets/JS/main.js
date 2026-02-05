23eb2ac7325f94c0a08e38f2d3dc80cb08fca9ed
Francesca Golzio <francescagolzio@gmail.com>
2026-02-05 16:04:12 +0100

    add list

--- mail/assets/JS/main.js
+++ mail/assets/JS/main.js
@@ -5,6 +5,10 @@
 /* DADI */ 
 /* variabili | array | prompt | operatori js | console.log() | for() */ 
 
+const party_guest_list = [ 
+  'paolino.paperino@duckmail.com','paperon.depaperoni@duckmail.com','mikey.mouse@topolinmail.com','bruce.wayne@gothmail.com','freccia.verde@greenmail.com','jones.indiana@relichmail.com','james.bond@007mail.com','arthur.pendragon@camelmail.com' ]
+
+
 /* Creo una lista di invitati (contiene 'email') */ 
 
 /* Chiedo all'Utente la sua 'email' */
