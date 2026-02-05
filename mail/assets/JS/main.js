e8af478a6cf1546474a1ac572300b934e06ccd74
Francesca Golzio <francescagolzio@gmail.com>
2026-02-05 16:09:30 +0100

    ask and store user email

--- mail/assets/JS/main.js
+++ mail/assets/JS/main.js
@@ -5,13 +5,13 @@
 /* DADI */ 
 /* variabili | array | prompt | operatori js | console.log() | for() */ 
 
+/* Creo una lista di invitati (contiene 'email') */ 
 const party_guest_list = [ 
   'paolino.paperino@duckmail.com','paperon.depaperoni@duckmail.com','mikey.mouse@topolinmail.com','bruce.wayne@gothmail.com','freccia.verde@greenmail.com','jones.indiana@relichmail.com','james.bond@007mail.com','arthur.pendragon@camelmail.com' ]
 
-
-/* Creo una lista di invitati (contiene 'email') */ 
-
-/* Chiedo all'Utente la sua 'email' */
+/* Chiedo all'Utente la sua 'email' e la conservo in una variabile*/
+const user_email = prompt('Type your email, please');
+//console.log(user_email);
 
 /* Controllo se l'email fa parte della lista */
 
